7d0d0d2615a75345560d3d29664ab2b5e25afc11
sa_fujii <jyujyusa.68@gmail.com>
2023-12-11 11:39:25 +0900

    Initial Commit

---
+++ ios-training-fujii/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-training-fujii
+//
+//  Created by 藤井 紗良 on 2023/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
