b7db81c37c03f232ab92afc74d168f87517dc6c9
sa_fujii <jyujyusa.68@gmail.com>
2023-12-14 17:11:27 +0900

    feat: FirstViewControllerを追加し、初期画面に設定

---
+++ ios-training-fujii/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  ios-training-fujii
+//
+//  Created by 藤井 紗良 on 2023/12/14.
+//
+
+import Foundation
+import UIKit
+
+final class FirstViewController: UIViewController {
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.backgroundColor = UIColor.black
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.performSegue(withIdentifier: "toMainView", sender: nil)
+    }
+
+    
+    
+    
+}
