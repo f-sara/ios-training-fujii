0d19a652b74bce65fe10937203ab732c54a3da56
sa_fujii <jyujyusa.68@gmail.com>
2023-12-18 17:54:22 +0900

    test: その他の天気の場合とLabelのテストも追加

--- ios-training-fujiiTests/ios_training_fujiiTests.swift
+++ ios-training-fujiiTests/ios_training_fujiiTests.swift
@@ -9,20 +9,65 @@ import XCTest
 @testable import ios_training_fujii
 
 final class ios_training_fujiiTests: XCTestCase {
+    var mainViewController: MainViewController = MainViewController()
+    var weatherModelMock: WeatherModelMock = WeatherModelMock()
+    
+    override func setUp() {
+        super.setUp()
+        mainViewController = MainViewController()
+        weatherModelMock = WeatherModelMock()
+    }
     
     func testWeatherConditionSunny() {
-        let viewController = MainViewController()
-        let weatherModelMock = WeatherModelMock()
-        viewController.weatherModel = weatherModelMock
+        mainViewController.weatherModel = weatherModelMock
 
         weatherModelMock.fetchWeatherAPIHandler = { _ in
             return WeatherDataModel(date: Date(), weatherCondition: .sunny, maxTemperature: 20, minTemperature: 10)
         }
         
-        viewController.reloadWeather(area: "tokyo")
+        mainViewController.reloadWeather(area: "tokyo")
         
-        XCTAssertEqual(viewController.weatherImageView.image, UIImage(named: "sunny"))
-        XCTAssertEqual(viewController.weatherImageView.tintColor, .red)
+        XCTAssertEqual(mainViewController.weatherImageView.image, UIImage(named: "sunny"))
+        XCTAssertEqual(mainViewController.weatherImageView.tintColor, .red)
+    }
+    
+    func testWeatherConditionRainy() {
+        mainViewController.weatherModel = weatherModelMock
+
+        weatherModelMock.fetchWeatherAPIHandler = { _ in
+            return WeatherDataModel(date: Date(), weatherCondition: .rainy, maxTemperature: 20, minTemperature: 10)
+        }
+        
+        mainViewController.reloadWeather(area: "tokyo")
+        
+        XCTAssertEqual(mainViewController.weatherImageView.image, UIImage(named: "rainy"))
+        XCTAssertEqual(mainViewController.weatherImageView.tintColor, .blue)
+    }
+    
+    func testWeatherConditionCloudy() {
+        mainViewController.weatherModel = weatherModelMock
+
+        weatherModelMock.fetchWeatherAPIHandler = { _ in
+            return WeatherDataModel(date: Date(), weatherCondition: .cloudy, maxTemperature: 20, minTemperature: 10)
+        }
+        
+        mainViewController.reloadWeather(area: "tokyo")
+        
+        XCTAssertEqual(mainViewController.weatherImageView.image, UIImage(named: "cloudy"))
+        XCTAssertEqual(mainViewController.weatherImageView.tintColor, .gray)
+    }
+    
+    func testWeatherTemperatureLabel() {
+        mainViewController.weatherModel = weatherModelMock
+
+        weatherModelMock.fetchWeatherAPIHandler = { _ in
+            return WeatherDataModel(date: Date(), weatherCondition: .sunny, maxTemperature: 20, minTemperature: 10)
+        }
+        
+        mainViewController.reloadWeather(area: "tokyo")
+        
+        XCTAssertEqual(mainViewController.maxTemperatureLabel.text, "20")
+        XCTAssertEqual(mainViewController.minTemperatureLabel.text, "10")
     }
     
 }
