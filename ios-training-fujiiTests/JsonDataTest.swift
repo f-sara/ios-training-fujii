ab6479b0e73f2a604ba2c25ede3827049902cbec
sa_fujii <jyujyusa.68@gmail.com>
2023-12-19 10:24:10 +0900

    test: Jsonのエンコード、デコードのテストを追加

---
+++ ios-training-fujiiTests/JsonDataTest.swift
@@ -0,0 +1,86 @@
+//
+//  JsonDataTest.swift
+//  ios-training-fujiiTests
+//
+//  Created by 藤井 紗良 on 2023/12/18.
+//
+
+import XCTest
+@testable import ios_training_fujii
+
+final class JsonDataTest: XCTestCase {
+    var weatherModelMock: WeatherModelMock = WeatherModelMock()
+    
+    let apiRequest = """
+        {
+            "area": "tokyo",
+            "date": "2020-04-01T12:00:00+09:00"
+        }
+    """
+    
+    let apiResponse = """
+        {
+            "max_temperature":25,
+            "date":"2020-04-01T12:00:00+09:00",
+            "min_temperature":7,
+            "weather_condition":"cloudy"
+        }
+    """
+    
+    override func setUp() {
+        super.setUp()
+        weatherModelMock = WeatherModelMock()
+    }
+     
+    
+    func testEncodeAPIRequest() {
+        let encodeMock: WeatherDataEncodeMock = WeatherDataEncodeMock()
+        let date = Date()
+        let encodeData: String
+        
+        encodeMock.encodeAPIRequestHandler = { request in
+            XCTAssertEqual(request.area, "tokyo")
+            XCTAssertEqual(request.date, date)
+            return self.apiRequest
+        }
+
+        do {
+            encodeData = try encodeMock.encodeAPIRequest(request: WeatherAPIRequest(area: "tokyo", date: date))
+        } catch {
+            encodeData = ""
+        }
+        
+        XCTAssertEqual(encodeData, self.apiRequest)
+    }
+    
+    func testDecodeAPIResponse() {
+        let decodeMock: WeatherDataDecodeMock = WeatherDataDecodeMock()
+        let date = Date()
+        let decodeData: WeatherDataModel
+        
+        let apiResponse = """
+            {
+                "max_temperature":25,
+                "date":"\(date)",
+                "min_temperature":7,
+                "weather_condition":"cloudy"
+            }
+        """
+        
+        decodeMock.decodeAPIResponseHandler = { response in
+            XCTAssertEqual(response, apiResponse)
+            return WeatherDataModel(date: date, weatherCondition: .cloudy, maxTemperature: 25, minTemperature: 7)
+        }
+        
+        do {
+            decodeData = try decodeMock.decodeAPIResponse(responseData: apiResponse)
+        } catch {
+            decodeData = WeatherDataModel(date: Date(timeInterval: 60, since: .now), weatherCondition: .sunny, maxTemperature: 10, minTemperature: 10)
+        }
+        
+        XCTAssertEqual(decodeData.date, date)
+        XCTAssertEqual(decodeData.weatherCondition, .cloudy)
+        XCTAssertEqual(decodeData.maxTemperature, 25)
+        XCTAssertEqual(decodeData.minTemperature, 7)
+    }
+}
